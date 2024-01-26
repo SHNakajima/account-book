7fb5088d5a8925aed6fc38a65bdeac829543664a
SHNakajima <48920484+SHNakajima@users.noreply.github.com>
2024-01-27 00:20:23 +0900

    fix: login page

--- laravel/resources/js/Pages/Auth/LineLogin.jsx
+++ laravel/resources/js/Pages/Auth/LineLogin.jsx
@@ -1,23 +1,20 @@
-import GuestLayout from "@/Layouts/GuestLayout";
+import { useEffect } from 'react';
+import Checkbox from '@/Components/Checkbox';
+import GuestLayout from '@/Layouts/GuestLayout';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
 
-export default function LineLogin({ lineRedirectUrl }) {
+export default function Login({ lineRedirectUrl }) {
     return (
         <GuestLayout>
             <Head title="Log in" />
-
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                }}
-            >
-                <div>
-                    <h1>Welcome to Our Website</h1>
-                    <p>Please log in to continue.</p>
-                    <a href={lineRedirectUrl}>Login</a>
-                </div>
+            <div className="container mt-12 flex items-center">
+                <a href={lineRedirectUrl} className='max-h-fit max-w-fit hover:opacity-70'>
+                    <img className='h-11' src="/images/line/btn_login_base.png" alt="line login button" />
+                </a>
             </div>
         </GuestLayout>
     );
