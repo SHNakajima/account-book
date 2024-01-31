ed7aa7d3bfd1f0a51c7020cbb35721443f227a6a
SHNakajima <48920484+SHNakajima@users.noreply.github.com>
2024-01-31 18:06:38 +0900

    fix

--- laravel/resources/js/Components/DropDownHeadless.jsx
+++ laravel/resources/js/Components/DropDownHeadless.jsx
@@ -1,16 +1,24 @@
 import { Menu } from '@headlessui/react'
 
 export default function DropDownHeadless( Items, title ) {
-  return (
-    <Menu>
-      <Menu.Button>{title}</Menu.Button>
-      <Menu.Items>
-            {Object.values(Items).map(item => (
-                <Menu.Item key={item.id}>
-                    {item.name}
-                </Menu.Item>
-            ))}
-      </Menu.Items>
-    </Menu>
-  )
+    return (
+        <Menu>
+            <Menu.Button>{title}</Menu.Button>
+            <Menu.Items>
+                {Object.values(Items).map((item) => (
+                    <Menu.Item key={item.href} as={Fragment}>
+                        {({ active }) => (
+                            <a
+                                href={item.href}
+                                className={`${active ? 'bg-blue-500 text-white' : 'bg-white text-black'
+                                    }`}
+                            >
+                                {item.label}
+                            </a>
+                        )}
+                    </Menu.Item>
+                ))}
+            </Menu.Items>
+        </Menu>
+    )
 }
