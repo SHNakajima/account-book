a93d4d01e5d83bf9559ad612aa13a8bdd07e03eb
SHNakajima <dev.shnakajima@gmail.com>
2024-02-08 23:21:49 +0900

    fix: style

--- laravel/resources/js/Components/ApplicationIcon.jsx
+++ laravel/resources/js/Components/ApplicationIcon.jsx
@@ -1,9 +1,18 @@
 export default function ApplicationIcon({ className, ...props }) {
   return (
-    <img
-      {...props}
-      className={`${className}`}
-      src="/images/icon/app_icon.png"
-    />
+    <>
+      <img
+        {...props}
+        className={`${className}`}
+        src="/images/icon/app_icon.png"
+      />
+      <style>
+        {`
+          .filter-shadow {
+            filter: drop-shadow(0 0 16px black);
+          }
+        `}
+      </style>
+    </>
   );
 }
