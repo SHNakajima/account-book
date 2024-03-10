c57e37709deaa6e270ad03238b6b7f74ac53562d
SHNakajima <dev.shnakajima@gmail.com>
2024-03-10 14:55:37 +0900

    refactor: route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,77 +1,83 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use App\Http\Controllers\Auth\LineOAuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LiffController;
-use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\{
+    Auth\LineOAuthController,
+    CategoryController,
+    DashboardController,
+    LiffController,
+    ProfileController,
+    TransactionController,
+    LineWebhookController
+};
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-
-// local test routes
+// ローカル環境専用ルート
 if (config('app.env') == 'local') {
     Route::get('/login/test', function () {
         Auth::login(User::find(1));
         return redirect()->route('dashboard');
     });
+
     Route::get('/chat/test', 'App\Services\ChatGPTService@test')->name('chat.test');
 }
 
+// 基本ルート
 Route::get('/', function () {
     return redirect()->route(RouteServiceProvider::HOME);
 });
 
+// LINE OAuth 2.1
+Route::prefix('auth/line')->controller(LineOAuthController::class)->group(function () {
+    Route::get('/', 'redirectToProvider')->name('line.login');
+    Route::get('/callback', 'handleProviderCallback');
+});
 
-// line OAuth 2.1
-Route::prefix('auth/line')
-    ->controller(LineOAuthController::class)
-    ->group(function () {
-        // LINEの認証画面に遷移
-        Route::get('/', 'redirectToProvider')->name('line.login');
-        // 認証後にリダイレクトされるURL(コールバックURL)
-        Route::get('/callback', 'handleProviderCallback');
-    });
+// Line messaging API
+Route::post('/line/webhook/message', [LineWebhookController::class, 'webhook'])->name('line.webhook.message');
 
-// Line messaging api
-Route::post('/line/webhook/message', 'App\Http\Controllers\LineWebhookController@webhook')->name('line.webhook.message');
-
+// 認証が必要なルート
 Route::middleware('auth')->group(function () {
     Route::get('/welcome', function () {
         return Inertia::render('Welcome');
     })->name('welcome');
+
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    // LIFF エンドポイント
     Route::get('/liff', [LiffController::class, 'index'])->name('liff.index');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // プロフィール関連
+    Route::controller(ProfileController::class)->prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
 
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::post('/categories', [CategoryController::class, 'init'])->name('categories.init');
-    Route::post('/categories/init', [CategoryController::class, 'create'])->name('categories.create');
-    Route::delete('/categories', [CategoryController::class, 'destroy'])->name('categories.destroy');
-    Route::patch('/categories', [CategoryController::class, 'merge'])->name('categories.merge');
-    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-    Route::patch('/transactions', [TransactionController::class, 'update'])->name('transactions.update');
-    Route::delete('/transactions', [TransactionController::class, 'destroy'])->name('transactions.destroy');
+    // カテゴリ関連
+    Route::controller(CategoryController::class)->prefix('categories')->name('categories.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'init')->name('init');
+        Route::post('/init', 'create')->name('create');
+        Route::delete('/', 'destroy')->name('destroy');
+        Route::patch('/', 'merge')->name('merge');
+    });
+
+    // トランザクション関連
+    Route::controller(TransactionController::class)->prefix('transactions')->name('transactions.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
 });
 
 require __DIR__ . '/auth.php';
