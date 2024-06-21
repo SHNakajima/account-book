9c9101edd22801e899e6d60c2a36dca1754dbbdd
SHNakajima <dev.shnakajima@gmail.com>
2024-06-22 00:41:40 +0900

    fix: vite env path

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import path from 'path';
 
-export default defineConfig({
+export default defineConfig(() => {
+  const envFilePath = path.resolve(__dirname, './env/phpenv');
+  const env = dotenv.config({ path: envFilePath });
+  dotenvExpand.expand(env);
+
+  return {
+    define: {
+      'process.env': env,
+    },
     plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            ssr: 'resources/js/ssr.jsx',
-            refresh: true,
-        }),
-        react(),
+      laravel({
+        input: 'resources/js/app.jsx',
+        ssr: 'resources/js/ssr.jsx',
+        refresh: true,
+      }),
+      react(),
     ],
+  };
 });
